c6faddb87b404632880f3063f6003b7f9b0403d7
digpal singh mandloi <digpalsinghmandloi10@gmail.com>
2023-07-31 13:31:41 +0530

    protect signIn-signup

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,24 +1,29 @@
 import SignIn from "@/components/SignIn"
 import { buttonVariants } from "@/components/ui/Button"
+import { getauthSession } from "@/lib/auth"
 import { cn } from "@/lib/utils"
 import { ChevronLeft } from "lucide-react"
 import Link from "next/link"
-import { FC } from "react"
+import { redirect } from "next/navigation"
 
 
-const Page: FC = () => {
+const page = async () => {
+    const session = await getauthSession()
+    if (session) {
+        redirect('/')
+    }
     return (
         <div className="absolute inset-0 ">
             <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20 ">
                 <Link className={cn(buttonVariants({ variant: 'ghost' }), 'self-start -mt-10')} href='/'>
-                  <ChevronLeft className="mr-2 h-4 w-4" />  Home
+                    <ChevronLeft className="mr-2 h-4 w-4" />  Home
                 </Link>
-        
+
                 <SignIn />
             </div>
         </div>
     )
 }
 
-export default Page
+export default page
 
